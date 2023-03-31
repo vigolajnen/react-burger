e94666726af2da798dcd045b3bd8e436756ddd35
Vigolajnen Anna <vigolajnen@gmail.com>
2023-04-01 01:32:00 +0300

    переделала табы

--- src/components/burger-ingredients/BurgerIngredients.jsx
+++ src/components/burger-ingredients/BurgerIngredients.jsx
@@ -6,9 +6,27 @@ import BurgerIngredientsItem from '../burger-ingredients-item/BurgerIngredientsI
 import stylesIngredients from './BurgerIngredients.module.css';
 
 export default class BurgerIngredients extends React.Component {
-  render() {
-    const tabs = document.querySelectorAll('a[href*="#"]');
+  state = {
+    current: 'Булки',
+  };
 
+  activeTab = (tab) => {
+    this.setState({ current: tab });
+
+    let type = 'bun';
+    if (tab === 'Соусы') {
+      type = 'sauce';
+    } else if (tab === 'Начинки') {
+      type = 'main';
+    }
+
+    document.querySelector('[data-value="' + type + '"]').scrollIntoView({
+      behavior: 'smooth',
+      inline: 'start',
+    });
+  };
+
+  render() {
     const bunIngredients = this.props.ingredients.filter(
       (item) => item.type === 'bun',
     );
@@ -19,60 +37,34 @@ export default class BurgerIngredients extends React.Component {
       (item) => item.type === 'sauce',
     );
 
-    const handelTabClick = () => {
+    const arrSort = bunIngredients
+      .concat(sauceIngredients)
+      .concat(mainIngredients);
 
-      function tabActive(e) {
-        console.log(e.currentTarget);
-
-        const blockID = e.currentTarget.getAttribute('href').substr(1);
-
-        document.getElementById(blockID).scrollIntoView({
-          behavior: 'smooth',
-          block: 'center',
-          inline: 'start',
-        });
-      }
-
-      for (var i = 0; i < tabs.length; i++) {
-        tabs[i].addEventListener('click', tabActive, false);
-      }
-    };
-
-    const [current] = 'Булки';
-    // const [current, setCurrent] = React.useState('Булки');
     return (
       <section>
         <div className={stylesIngredients.header}>
-          <a href='#bun' className={stylesIngredients.link}>
-            <Tab
-              value='Булки'
-              active={current === 'Булки'}
-              onClick={handelTabClick}
-              // cards={bunIngredients}
-            >
-              Булки
-            </Tab>
-          </a>
-          <a href='#sauce' className={stylesIngredients.link}>
-            <Tab
-              value='Соусы'
-              active={current === 'Соусы'}
-              onClick={handelTabClick}
-              // cards={sauceIngredients}
-            >
-              Соусы
-            </Tab>
-          </a>
-          <a href='#main' className={stylesIngredients.link}>
-            <Tab
-              value='Начинки'
-              active={current === 'Начинки'}
-              onClick={handelTabClick}
-              // cards={mainIngredients}
-            >
-              Начинки
-            </Tab>
-          </a>
+          <Tab
+            value='Булки'
+            active={this.state.current === 'Булки'}
+            onClick={this.activeTab}
+          >
+            Булки
+          </Tab>
+          <Tab
+            value='Соусы'
+            active={this.state.current === 'Соусы'}
+            onClick={this.activeTab}
+          >
+            Соусы
+          </Tab>
+          <Tab
+            value='Начинки'
+            active={this.state.current === 'Начинки'}
+            onClick={this.activeTab}
+          >
+            Начинки
+          </Tab>
         </div>
         <div style={{ height: '450px', overflow: 'hidden', padding: '10px 0' }}>
           <div
@@ -80,38 +72,18 @@ export default class BurgerIngredients extends React.Component {
             style={{
               height: '100%',
               overflow: 'auto',
+              padding: '0 10px',
             }}
           >
-            <div id='bun' className={stylesIngredients.grid}>
-              {bunIngredients.map((item) => (
+            <div className={stylesIngredients.grid}>
+              {arrSort.map((item) => (
                 <BurgerIngredientsItem
                   key={item._id}
                   item={item}
                   text={item.name}
                   price={item.price}
                   thumbnail={item.image}
-                />
-              ))}
-            </div>
-            <div id='main' className={stylesIngredients.grid}>
-              {mainIngredients.map((item) => (
-                <BurgerIngredientsItem
-                  key={item._id}
-                  item={item}
-                  text={item.name}
-                  price={item.price}
-                  thumbnail={item.image}
-                />
-              ))}
-            </div>
-            <div id='sauce' className={stylesIngredients.grid}>
-              {sauceIngredients.map((item) => (
-                <BurgerIngredientsItem
-                  key={item._id}
-                  item={item}
-                  text={item.name}
-                  price={item.price}
-                  thumbnail={item.image}
+                  type={item.type}
                 />
               ))}
             </div>
