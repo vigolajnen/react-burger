004137a52b51944264a48556d49a825653299bbd
Vigolajnen Anna <vigolajnen@gmail.com>
2023-04-20 22:24:18 +0300

    Подключила Redux Devtools к проекту

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/app/App';
+import { compose, createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
 import reportWebVitals from './reportWebVitals';
+import { rootReducer } from './services/reducers';
+import thunk from 'redux-thunk';
+import App from './components/app/App';
+
+// style
+import './index.css';
+
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
+
+const enhancer = composeEnhancers(applyMiddleware(thunk));
+const store = createStore(rootReducer, composeWithDevTools(enhancer));
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
