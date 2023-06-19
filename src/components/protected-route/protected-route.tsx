a3e09dc81ff3615d61a9ded534a135a4b9d81435
Vigolajnen Anna <vigolajnen@gmail.com>
2023-06-19 16:56:23 +0300

    убрала console.log

--- src/components/protected-route/protected-route.tsx
+++ src/components/protected-route/protected-route.tsx
@@ -12,10 +12,6 @@ const ProtectedRoute = ({ anonymous = false, children }: Props) => {
   const location = useLocation();
   const localToken = !getCookie('token');
   const from = location.state?.from || '/';
-  const { isLoggedIn } = useSelector((store) => store.user);
-  console.log(anonymous && isLoggedIn);
-  console.log(!anonymous && !isLoggedIn);
-
 
   if (!anonymous && localToken) {
     return <Navigate to='/login' state={{ from: location }} replace />;
